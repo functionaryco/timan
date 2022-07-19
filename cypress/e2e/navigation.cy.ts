fda25cbcfcc14ccfa3ede6cef048b5eb3a8a96a6
amit-g0swami <goswamiempire@gmail.com>
2022-07-19 17:25:59 +0530

    fix:cypress error

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,10 +1,13 @@
+/// <reference types="cypress" />
+
 describe('Rendering and navigation', () => {
   it('should navigate between pages without crashing', () => {
-    cy.visit('http://localhost:3000');
-    cy.findAllByText(/hello/i).should('exist');
+    cy.visit('http://localhost:3000/');
     cy.findAllByText(/style guide/i).click();
     cy.findAllByText(/color palette/i).should('exist');
     cy.findAllByText(/home/i).click();
-    cy.findAllByText(/hello/i).should('exist');
+    cy.findAllByText(/home/i).should('exist');
   });
 });
+
+export { }
