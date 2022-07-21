982b2e31aed75e213ffa0a854197e1a47e46d940
amit-g0swami <goswamiempire@gmail.com>
2022-07-21 08:03:24 +0530

    feat:added react-time-picker

--- src/components/Battery.tsx
+++ src/components/Battery.tsx
@@ -4,11 +4,12 @@ import { Box, Container, Input, Text } from 'theme-ui';
 import Bar from './Bar';
 import Percentage from './Percentage';
 import Time from './Time';
-import 'react-day-picker/dist/style.css';
-import { addDays, format } from 'date-fns';
-import { DateRange, DayPicker } from 'react-day-picker';
+// import 'react-day-picker/dist/style.css';
+// import { addDays, format } from 'date-fns';
+// import { DateRange, DayPicker } from 'react-day-picker';
+import TimePicker from 'react-time-picker';
 
-const pastMonth = new Date(2020, 10, 15);
+// const pastMonth = new Date(2020, 10, 15);
 
 // const Index = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 // const Slots = Index.map((hour) => {
@@ -16,29 +17,19 @@ const pastMonth = new Date(2020, 10, 15);
 // });
 
 function Battery() {
-    const defaultSelected: DateRange = {
-        from: pastMonth,
-        to: addDays(pastMonth, 4)
-    };
-    const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
-    let footer = <p>Please pick the first day.</p>;
-    if (range?.from) {
-        if (!range.to) {
-            footer = <p>{format(range.from, 'PPP')}</p>;
-        } else if (range.to) {
-            footer = (
-                <p>
-                    {"Start Date " + format(range.from, 'PPP')}<br />{"End Date " + format(range.to, 'PPP')}
-                </p>
-            );
-        }
-    }
+    const [value, onChange] = useState('10:00');
+    // const defaultSelected: DateRange = {
+    //     from: pastMonth,
+    //     to: addDays(pastMonth, 4)
+    // };
+    // const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
     const [showCalender, setShowCalender] = useState(false);
 
-    const [startTime, setStartTime] = useState<any>("")
-    const [endTime, setEndTime] = useState<any>("")
+    // const [startTime, setStartTime] = useState<any>("")
+    // const [endTime, setEndTime] = useState<any>("")
     // const [hour, setHour] = useState<any>(12);
-    const hour = (startTime && endTime != null) ? (endTime - startTime) : "";
+    // const hour = (startTime && endTime != null) ? (endTime - startTime) : "";
+    const hour = 12;
     var today = new Date();
     var time = today.getHours();
     var min = today.getMinutes();
@@ -46,24 +37,40 @@ function Battery() {
     for (var i = 1; i < hour; i++) {
         arr.push(i);
     }
+    // useEffect(() => {
+    //     localStorage.setItem("Time", JSON.stringify({ startTime, endTime }));
+    // }, [hour])
+    const handleTime = () => {
 
-    useEffect(() => {
-        localStorage.setItem("Time", JSON.stringify({ startTime, endTime }));
-    }, [hour])
-
+    }
+    // let footer = <p>Please pick the first day.</p>;
+    // if (range?.from) {
+    //     if (!range.to) {
+    //         footer = <p>{format(range.from, 'PPP')}</p>;
+    //     } else if (range.to) {
+    //         footer = (
+    //             <p>
+    //                 {"Start Date " + format(range.from, 'PPP')}<br />{"End Date " + format(range.to, 'PPP')}
+    //             </p>
+    //         );
+    //     }
+    // }
 
     return (
         <>
             {showCalender &&
-                <Container style={{ position: 'absolute', zIndex: '1', top: "180px" }}>
-                    <DayPicker
-                        mode="range"
-                        defaultMonth={pastMonth}
-                        selected={range}
-                        footer={footer}
-                        onSelect={setRange}
-                    />
-                </Container>
+                // <Container style={{ position: 'absolute', zIndex: '1', top: "180px" }}>
+                //                 <DayPicker
+                //                     mode="range"
+                //                     defaultMonth={pastMonth}
+                //                     selected={range}
+                //                     footer={footer}
+                //                     onSelect={setRange}
+                //                 />
+                //             </Container> 
+                //             <TimePicker onChange={onChange} value={value} />
+                //         </Container>
+                <Container><TimePicker onChange={onChange} value={value} /></Container>
             }
 
             <Container style={{ width: '70vw' }}>
