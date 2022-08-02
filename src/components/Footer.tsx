8333d8844955b2b5662de4ee62c56dfd0b6aed95
amit-g0swami <goswamiempire@gmail.com>
2022-08-02 16:10:58 +0530

    style:styling:

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 /** @jsxImportSource theme-ui */
 import React, { useState } from 'react'
-import { Box, Text,Image } from 'theme-ui'
+import { Box, Text, Image } from 'theme-ui'
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
 function Footer() {
-              const musicTracks = [
+  const musicTracks = [
     {
       name: "Memories",
       src: "https://www.bensound.com/bensound-music/bensound-memories.mp3"
@@ -70,7 +70,7 @@ function Footer() {
   ];
 
   const [trackIndex, setTrackIndex] = useState(0);
-    const [openPlayer, setOpenPlayer] = useState(false);
+  const [openPlayer, setOpenPlayer] = useState(false);
 
   const handleClickPrevious = () => {
     setTrackIndex((currentTrack) =>
@@ -83,71 +83,75 @@ function Footer() {
       currentTrack < musicTracks.length - 1 ? currentTrack + 1 : 0
     );
   };
-    return (
-<>
- {openPlayer && 
- <Box sx={{ width: '100%', height: '100%', position: 'absolute', backgroundColor: "rgba(50, 37, 37, 0.6)", top: "0", bottom: "0", left: '0', right: '0' }}>
- <Box sx={{display:'flex',marginTop:'30vh',position:'absolute',marginLeft:"37vw",justifyContent:'flex-end', '@media screen and (max-width: 720px)': {
-                        marginLeft:'75vw !important'
-                        },}}> 
-   <Image src="https://img.icons8.com/ios/35/d21e1e/multiply.png" sx={{cursor: 'pointer',borderRadius:'50%',zIndex:"1" }} alt="cross" onClick={() => setOpenPlayer(!openPlayer)} />
-                     <AudioPlayer
-        sx={{  width: '30vw', height: '30vh', position: 'absolute', backgroundColor: '#E4E4E4', padding: '50px', borderRadius: '8px',color:"black",
-                        '@media screen and (max-width: 720px)': {
-                            width: '70vw !important', height: '23vh !important', 
-                            padding: '30px !important',
-                        }, }}
+  return (
+    <>
+      {openPlayer &&
+        <Box sx={{ width: '100%', height: '100%', position: 'absolute', backgroundColor: "rgba(50, 37, 37, 0.6)", top: "0", bottom: "0", left: '0', right: '0' }}>
+          <Box sx={{
+            display: 'flex', marginTop: '30vh', position: 'absolute', marginLeft: "37vw", justifyContent: 'flex-end', '@media screen and (max-width: 720px)': {
+              marginLeft: '75vw !important'
+            },
+          }}>
+            <Image src="https://img.icons8.com/ios/35/d21e1e/multiply.png" sx={{ cursor: 'pointer', borderRadius: '50%', zIndex: "1" }} alt="cross" onClick={() => setOpenPlayer(!openPlayer)} />
+            <AudioPlayer
+              sx={{
+                width: '30vw', height: '30vh', position: 'absolute', backgroundColor: '#E4E4E4', padding: '50px', borderRadius: '8px', color: "black",
+                '@media screen and (max-width: 720px)': {
+                  width: '70vw !important', height: '23vh !important',
+                  padding: '30px !important',
+                },
+              }}
               autoPlay
-        // layout="horizontal"
-        src={musicTracks[trackIndex].src}
-        onPlay={(e) => console.log("onPlay")}
-        showSkipControls={true}
-        showJumpControls={false}
-        header={`${musicTracks[trackIndex].name}`}
-        // footer="All music from: www.bensound.com"
-        onClickPrevious={handleClickPrevious}
-        onClickNext={handleClickNext}
-        onEnded={handleClickNext}
-        // other props here
-      />
- </Box>
-                    </Box>
-        }
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: "47px", flexDirection: 'row', gap: 'auto' }}>
-            <Box sx={{
-                display: 'flex', alignItems: 'center', justifyContent: "center", gap: '21px',
-                '@media screen and (max-width: 720px)': {
-                    gap: '10px !important'
-                },
-            }}>
-                <Box sx={{
-                    width: '29px', height: '29px', backgroundColor: "#4F4F4F", borderRadius: '50%',
-                    '@media screen and (max-width: 720px)': {
-                        height: '19px !important'
-                    },
-                }}
-                ></Box>
-                <Text sx={{
-                    width: '364px', height: "27px", fontFamily: 'Inter', fontWeight: '400', fontSize: '22px', lineHeight: '27px', color: "#fff",
-                    '@media screen and (max-width: 720px)': {
-                        fontSize: '18px !important',
-                        width: "264px !important"
-                    },
-                }}
-                >{openPlayer ? musicTracks[trackIndex].name : "Koramangala Thirdwave Coffeshop"}</Text>
-            </Box>
-            <Text sx={{
-                width: '52px', height: "17px", fontFamily: 'Inter', fontWeight: '400', fontSize: '14px', lineHeight: '17px', color: "#fff", opacity: "0.5",cursor:"pointer",
-                '@media screen and (max-width: 720px)': {
-                    fontSize: '18px !important',
-                },
-            }}
-            onClick={() => setOpenPlayer(!openPlayer)}
-            >Change</Text>
+              // layout="horizontal"
+              src={musicTracks[trackIndex].src}
+              onPlay={(e) => console.log("onPlay")}
+              showSkipControls={true}
+              showJumpControls={false}
+              header={`${musicTracks[trackIndex].name}`}
+              // footer="All music from: www.bensound.com"
+              onClickPrevious={handleClickPrevious}
+              onClickNext={handleClickNext}
+              onEnded={handleClickNext}
+            // other props here
+            />
+          </Box>
         </Box>
-</>
-        
-    )
+      }
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: "47px", flexDirection: 'row', gap: 'auto' }}>
+        <Box sx={{
+          display: 'flex', alignItems: 'center', justifyContent: "center", gap: '21px',
+          '@media screen and (max-width: 720px)': {
+            gap: '10px !important'
+          },
+        }}>
+          <Box sx={{
+            width: '29px', height: '29px', backgroundColor: "#4F4F4F", borderRadius: '50%',
+            '@media screen and (max-width: 720px)': {
+              height: '19px !important'
+            },
+          }}
+          ></Box>
+          <Text sx={{
+            width: '364px', height: "27px", fontWeight: '400', fontSize: '22px', lineHeight: '27px', color: "#fff", fontStyle: 'normal',
+            '@media screen and (max-width: 720px)': {
+              fontSize: '18px !important',
+              width: "264px !important"
+            },
+          }}
+          >{openPlayer ? musicTracks[trackIndex].name : "Koramangala Thirdwave Coffeshop"}</Text>
+        </Box>
+        <Text sx={{
+          width: '52px', height: "17px", fontWeight: '400', fontSize: '14px', lineHeight: '17px', color: "#fff", opacity: "0.5", cursor: "pointer",
+          '@media screen and (max-width: 720px)': {
+            fontSize: '18px !important',
+          },
+        }}
+          onClick={() => setOpenPlayer(!openPlayer)}
+        >Change</Text>
+      </Box>
+    </>
+
+  )
 }
 
 export default Footer
